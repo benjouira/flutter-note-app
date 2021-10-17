5949e2bea6851a804c4f8d8e90325f6a0d44cac8
Rabii <rabi3jouira@gmail.com>
2021-10-17 20:48:31 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:js';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:note_app/auth/login.dart';
-import 'package:note_app/auth/signup.dart';
-import 'package:note_app/home/homepage.dart';
+import 'package:flutter_apps/auth/login.dart';
+import 'package:flutter_apps/auth/signup.dart';
+import 'package:flutter_apps/crud/addnotes.dart';
+import 'package:flutter_apps/home/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,16 +18,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.purple,
+          // ignore: deprecated_member_use
           buttonColor: Colors.red,
-          textTheme: TextTheme(
+          textTheme: const TextTheme(
               headline1: TextStyle(fontSize: 20, color: Colors.white))),
       home: Login(),
       debugShowCheckedModeBanner: false,
       routes: {
         "login": (context) => Login(),
         "signup": (context) => Signup(),
-        "homepage": (context) => HomePage()
+        "homepage": (context) => HomePage(),
+        "addnotes": (context) => AddNotes(),
       },
     );
   }
